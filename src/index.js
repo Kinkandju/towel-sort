a028cd7ac9bb8adac78ab882f6c59da173933fdb
Ямщикова Евгения <31244230+Kinkandju@users.noreply.github.com>
2022-02-08 20:10:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
+module.exports = function towelSort(matrix) {
+	let newMatrix = [];
+	// undefined является свойством глобального объекта, то есть, это переменная в глобальной области видимости.
+	// Начальным значением undefined является примитивное значение undefined.
+	if (matrix === undefined || matrix.length === 0) {
+		return newMatrix;
+	}
 
-// You should implement your task here.
+	for (let i = 0; i < matrix.length; i++) {
+		if (i % 2 !== 0) {
+            // Метод reverse() на месте обращает порядок следования элементов массива.
+		    // Первый элемент массива становится последним, а последний — первым.
+			newMatrix.push(matrix[i].reverse());
+		} else if (i % 2 === 0) {
+			newMatrix.push(matrix[i]);
+		}
+	}
 
-module.exports = function towelSort (matrix) {
-  return [];
+	// Метод reduce() применяет функцию reducer к каждому элементу массива (слева-направо), возвращая одно результирующее значение.
+	let sortedMatrix = newMatrix.reduce((a, b) => {
+		// JavaScript метод concat() используется для объединения двух, или более массивов в один,
+		// при этом он не изменяет существующие массивы, а возвращает новый массив.
+		return a.concat(b);
+	});
+
+	return sortedMatrix;
 }
